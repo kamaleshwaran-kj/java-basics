a0033383e9629bd263ec79d2965e88c52d4631e1
kamaleshwaran-kj <kamaleshwarankj11@gmail.com>
2025-08-29 18:05:59 +0530

    ArrarList_Pr

--- main.java
+++ main.java
@@ -1,6 +1,20 @@
 // File: Main.java
+
+import java.util.ArrayList;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " Hello !");
+        }
+
+        ArrayList<String> a = new ArrayList<String>();
+
+        a.add("Myilvaganan");
+        a.add("Kamale");
+        a.reversed();
+        System.out.println(a.reversed());
+        System.out.println(a.getClass().getName());
+
     }
 }
